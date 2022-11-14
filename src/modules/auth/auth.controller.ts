f025a30a614f1b3a8e7a44e6a098c765f7a179de
Alex <dadi2015@ukr.net>
2022-11-14 11:37:14 +0200

    Bugfix in application - Improved auth service - Improved token service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -21,7 +21,7 @@ export class AuthController {
   @ApiTags('API')
   @ApiResponse({status: 200, type: AuthUserResponse})
   @Post("login")
-  login(@Body() dto: UserLoginDTO): Promise<any> {
+  login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
     return this.authService.loginUser(dto);
   }
 }
